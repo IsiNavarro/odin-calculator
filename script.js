87e713be5acbc2992683bc5a273dc48de6e745a2
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-01-28 21:18:19 +0100

    First functioning version

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
-const sampleString = "-12+23+34*4.5^56/5";
+const display = document.querySelector(".display");
+const numbersButtons = document.querySelectorAll(".numbers");
+const operandsButtons = document.querySelectorAll(".operand");
+const equal = document.querySelector("#equal");
+const dot = document.querySelector("#dot");
+const ac = document.querySelector("#ac");
+const operandList = ["+","-","*","**","/"];
 
+let lastOperand = true;
+let pressedDot = false; 
+let afterEqual = false;
+
+numbersButtons.forEach(button => button.addEventListener("click", ()=> {
+    if (afterEqual) clearScreen();
+    appendToDisplay(button.textContent);
+
+    lastOperand = false;
+    afterEqual = false
+}));
+dot.addEventListener("click", ()=>{
+    if (afterEqual) clearScreen();
+    if (!pressedDot){
+        appendToDisplay(".");
+        pressedDot = true;
+        afterEqual = false;
+    }
+})
+operandsButtons.forEach(button => button.addEventListener("click", ()=>{
+    if (afterEqual) clearScreen();
+    if (lastOperand){
+        if ((display.textContent == false) && (button.textContent == "-")){
+            appendToDisplay(button.textContent);
+        }
+    }
+    else{
+        if(button.textContent == "xy"){
+            appendToDisplay("^");
+        } else {appendToDisplay(button.textContent)}
+        lastOperand = true;
+        pressedDot = false
+    }
+    
+}));
+ac.addEventListener("click", () => clearScreen());
+equal.addEventListener("click", ()=>{
+    operate(display.textContent.trim())
+
+    afterEqual = true;
+});
+
+function clearScreen(){
+    display.textContent = "";
+
+    lastOperand = true;
+    pressedDot = false;
+}
+function appendToDisplay(str){
+    display.textContent += str;
+}
 //Need to make sure you can't type operands where they don't belong.
 //String has to come in perfect format. Input has to be clean.
 
@@ -33,6 +90,10 @@ function getOperandsAndNumbers(str){
             else{                               //operands (means up until then it was all a number)
                 if (str[i] == "^") {
                     operands.push("**");
+                } else if(str[i] == "x"){
+                    operands.push("*");
+                } else if(str[i] == "÷"){
+                    operands.push("/");
                 } else{
                     operands.push(str[i]);
                 }
@@ -49,15 +110,14 @@ function getOperandsAndNumbers(str){
 
 function operate(str){
     let [operands, numbers] = getOperandsAndNumbers(str);
-    console.log(operands, numbers)
 
     let a;
     let b;
     let index;
 
     /*Hierarchy:-Power
-                -Multiplication & Division
-                -Sum*/
+                -Multiplication & Division: from left to right
+                -Sum & Rest*/
 
     while (operands.findIndex(operand => operand == "**") != -1){
         index = operands.findIndex(operand => operand == "**");
@@ -87,7 +147,6 @@ function operate(str){
     for(let index = 0; index<operands.length; index++){
         if (operands[index] == "+"){
             a = numbers.splice(index, 1);
-            console.log(typeof a);
             b = numbers[index];
             numbers.splice(index, 1, sum(a, b));
 
@@ -103,10 +162,12 @@ function operate(str){
         }
     }
     
-
-    return [numbers, operands];
+    display.textContent = roundNumber(numbers[0], 4);
 }
 
+function roundNumber(num, dec) {
+    return Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
+}
 function power(a, b){
     return a**b;
 }
